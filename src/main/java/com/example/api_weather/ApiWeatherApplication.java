d1d7e390c3a3e17cfbf07d49d0e2c5aeab308f29
gosttavoo <gustavogoulart92@gmail.com>
2024-11-26 19:04:54 -0300

    fst setup

---
+++ src/main/java/com/example/api_weather/ApiWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWeatherApplication.class, args);
+	}
+
+}
